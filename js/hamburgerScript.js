61d9b9cf2902419f360cd605b37cc27ad8910af4
LukasSkaregard <coolo8462@gmail.com>
2022-05-31 08:56:59 +0200

    Färdiga webbsidan

---
+++ js/hamburgerScript.js
@@ -0,0 +1,11 @@
+function myFunction() {
+    var clickMenu = document.getElementById("clickmenu");
+    
+    if (clickMenu.style.display === "none") {
+            clickMenu.style.display = "block";
+    } 
+        
+    else {
+            clickMenu.style.display = "none";
+    }
+}
